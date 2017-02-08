d82e914cd8a9398a22c6f15689c5f535489d02aa
Andy Carson <andrew.carson@silverpond.com.au>
2017-02-08 13:59:46 +1100

    hmmmm

--- deploy.sh
+++ deploy.sh
@@ -13,4 +13,4 @@ git add .
 git commit -m "Deploy to Github Pages"
 
 echo "deploying"
-git push --force --quiet "https://${GITUB_API_KEY}@github.com/arcarson/gatsby-with-travis-ci.git" master:gh-pages > /dev/null 2>&1
+git push --force "https://${GITUB_API_KEY}@github.com/arcarson/gatsby-with-travis-ci.git" master:gh-pages
